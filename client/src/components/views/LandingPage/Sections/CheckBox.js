c1655743ea7112255dfafbf6b038cf98902af022
unknown <joonsun926@gmail.com>
2022-08-26 20:55:59 +0900

    Update CheckBox.js

--- client/src/components/views/LandingPage/Sections/CheckBox.js
+++ client/src/components/views/LandingPage/Sections/CheckBox.js
@@ -8,21 +8,20 @@ function CheckBox(props) {
 
   const handleToggle = (value) => {
     //누른 것의 Index를 구하고
-    const currentIndex = Checked.indexOf(value);
+    const currentIndex = Checked.indexOf(value); 
     //전체 Checked된 State에서  현재 누른 Checkbox가 이미 있다면
     const newChecked = [...Checked];
 
     // State 넣어준다.
+
+    // 없는 값을 넣으면 -1이 나온다
     if (currentIndex === -1) {
       newChecked.push(value);
       // 빼주고
     } else {
       newChecked.splice(currentIndex, 1);
     }
-    setChecked(newChecked);
-    props.handleFilters(newChecked);
-  };
-
+  
   const renderCheckboxLists = () =>
     props.list &&
     props.list.map((value, index) => (
