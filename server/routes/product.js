928e0188dfeb823b027cdab0b0cb93d3b2a3cf54
unknown <joonsun926@gmail.com>
2022-08-27 16:44:45 +0900

    Update product.js

--- server/routes/product.js
+++ server/routes/product.js
@@ -54,8 +54,6 @@ router.post("/products", (req, res) => {
 
   for (let key in req.body.filters) {
     if (req.body.filters[key].length > 0) {
-      console.log("key", key);
-
       if (key === "price") {
         findArgs[key] = {
           //Greater than equal
