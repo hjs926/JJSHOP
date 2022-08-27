ffea51936c42d69b0048ec0271a1da5a5096a2b3
unknown <joonsun926@gmail.com>
2022-08-27 17:47:14 +0900

    Update ProductImage.js

--- client/src/components/views/DetailProductPage/Sections/ProductImage.js
+++ client/src/components/views/DetailProductPage/Sections/ProductImage.js
@@ -1,31 +1,29 @@
-import React, { useState, useEffect } from 'react'
-import ImageGallery from 'react-image-gallery';
+import React, { useState, useEffect } from "react";
+import ImageGallery from "react-image-gallery";
 
 function ProductImage(props) {
+  const [Images, setImages] = useState([]);
 
-    const [Images, setImages] = useState([])
+  useEffect(() => {
+    if (props.detail.images && props.detail.images.length > 0) {
+      let images = [];
 
-    useEffect(() => {
+      props.detail.images.map((item) => {
+        images.push({
+          original: `http://localhost:5000/${item}`,
+          thumbnail: `http://localhost:5000/${item}`,
+        });
+      });
+      setImages(images);
+    }
+  }, [props.detail]); // props.detail이 바뀌기때문에 리사이클 됨
 
-        if (props.detail.images && props.detail.images.length > 0) {
-            let images = []
-
-            props.detail.images.map(item => {
-                images.push({
-                    original: `http://localhost:5000/${item}`,
-                    thumbnail: `http://localhost:5000/${item}`
-                })
-            })
-            setImages(images)
-        }
-
-    }, [props.detail])
-
-    return (
-        <div>
-            <ImageGallery items={Images} />
-        </div>
-    )
+  // 렌더링 된다음에 위에꺼를 리사이클 해줌 다음 이미지 전달
+  return (
+    <div>
+      <ImageGallery items={Images} />
+    </div>
+  );
 }
 
-export default ProductImage
+export default ProductImage;
