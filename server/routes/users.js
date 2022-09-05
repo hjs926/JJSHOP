a1471400b60ce3eeef00be64049b27ba90918059
hjs926 <joonsun926@gmail.com>
2022-09-05 20:13:08 +0900

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -11,6 +11,21 @@ const async = require("async");
 //             User
 //=================================
 
+router.get("/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+    cart: req.user.cart,
+    history: req.user.history,
+  });
+});
+
 router.post("/register", (req, res) => {
   const user = new User(req.body);
 
