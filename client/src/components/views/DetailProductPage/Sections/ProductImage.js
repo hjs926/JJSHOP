c08d7af8cfe3061036ce5a434d8aae19e74af534
hjs926 <joonsun926@gmail.com>
2023-02-16 15:48:19 +0900

    update

--- client/src/components/views/DetailProductPage/Sections/ProductImage.js
+++ client/src/components/views/DetailProductPage/Sections/ProductImage.js
@@ -1,29 +1,31 @@
-import React, { useState, useEffect } from "react";
-import ImageGallery from "react-image-gallery";
+import React, { useState, useEffect } from 'react'
+import ImageGallery from 'react-image-gallery';
 
 function ProductImage(props) {
-  const [Images, setImages] = useState([]);
 
-  useEffect(() => {
-    if (props.detail.images && props.detail.images.length > 0) {
-      let images = [];
+    const [Images, setImages] = useState([])
 
-      props.detail.images.map((item) => {
-        images.push({
-          original: `http://localhost:5000/${item}`,
-          thumbnail: `http://localhost:5000/${item}`,
-        });
-      });
-      setImages(images);
-    }
-  }, [props.detail]); // props.detail이 바뀌기때문에 리사이클 됨
+    useEffect(() => {
 
-  // 렌더링 된다음에 위에꺼를 리사이클 해줌 다음 이미지 전달
-  return (
-    <div>
-      <ImageGallery items={Images} />
-    </div>
-  );
+        if (props.detail.images && props.detail.images.length > 0) {
+            let images = []
+
+            props.detail.images.map(item => {
+                images.push({
+                    original: `http://localhost:5000/${item}`,
+                    thumbnail: `http://localhost:5000/${item}`
+                })
+            })
+            setImages(images)
+        }
+
+    }, [props.detail])
+
+    return (
+        <div>
+            <ImageGallery items={Images} />
+        </div>
+    )
 }
 
-export default ProductImage;
+export default ProductImage
--- client/src/components/views/LandingPage/Sections/CheckBox.js
+++ client/src/components/views/LandingPage/Sections/CheckBox.js
@@ -1,52 +1,50 @@
-import React, { useState } from "react";
-import { Collapse, Checkbox } from "antd";
+import React, { useState } from 'react'
+import { Collapse, Checkbox } from 'antd';
 
 const { Panel } = Collapse;
 
 function CheckBox(props) {
-  const [Checked, setChecked] = useState([]);
 
-  const handleToggle = (value) => {
-    //누른 것의 Index를 구하고
-    const currentIndex = Checked.indexOf(value);
-    //전체 Checked된 State에서  현재 누른 Checkbox가 이미 있다면
-    const newChecked = [...Checked];
+    const [Checked, setChecked] = useState([])
 
-    // State 넣어준다.
+    const handleToggle = (value) => {
+        //누른 것의 Index를 구하고 
+        const currentIndex = Checked.indexOf(value)
+        //전체 Checked된 State에서  현재 누른 Checkbox가 이미 있다면 
+        const newChecked = [...Checked]
 
-    // 없는 값을 넣으면 -1이 나온다
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-      // 빼주고
-    } else {
-      newChecked.splice(currentIndex, 1);
+        // State 넣어준다. 
+        if (currentIndex === -1) {
+            newChecked.push(value)
+            // 빼주고 
+        } else {
+            newChecked.splice(currentIndex, 1)
+        }
+        setChecked(newChecked)
+        props.handleFilters(newChecked)
     }
-    setChecked(newChecked);
-    props.handleFilters(newChecked);
-  };
 
-  const renderCheckboxLists = () =>
-    props.list &&
-    props.list.map((value, index) => (
-      <React.Fragment key={index}>
-        <Checkbox
-          onChange={() => handleToggle(value._id)}
-          checked={Checked.indexOf(value._id) === -1 ? false : true}
-        />
-        <span>{value.name}</span>
-      </React.Fragment>
-    ));
 
-  return (
-    <div>
-      <Collapse defaultActiveKey={["0"]}>
-        <Panel header="Continents" key="1">
-          {renderCheckboxLists()}
-        </Panel>
-      </Collapse>
-    </div>
-  );
+
+    const renderCheckboxLists = () => props.list && props.list.map((value, index) => (
+        <React.Fragment key={index} >
+            <Checkbox onChange={() => handleToggle(value._id)}
+                checked={Checked.indexOf(value._id) === -1 ? false : true} />
+            <span>{value.name}</span>
+        </React.Fragment>
+    ))
+
+    return (
+        <div>
+            <Collapse defaultActiveKey={['0']} >
+                <Panel header="Continents" key="1">
+
+                    {renderCheckboxLists()}
+
+                </Panel>
+            </Collapse>
+        </div>
+    )
 }
 
-export default CheckBox;
+export default CheckBox
