add546e2edc620dd1e4033a08611a9ec94c63d87
hjs926 <joonsun926@gmail.com>
2023-03-11 13:52:16 +0900

    update

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -1,20 +1,20 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
-import { Menu, Icon, Badge } from 'antd';
-import axios from 'axios';
-import { USER_SERVER } from '../../../Config';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { Menu, Icon, Badge } from "antd";
+import axios from "axios";
+import { USER_SERVER } from "../../../Config";
+import { withRouter } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function RightMenu(props) {
-  const user = useSelector(state => state.user)
+  const user = useSelector((state) => state.user);
 
   const logoutHandler = () => {
-    axios.get(`${USER_SERVER}/logout`).then(response => {
+    axios.get(`${USER_SERVER}/logout`).then((response) => {
       if (response.status === 200) {
         props.history.push("/login");
       } else {
-        alert('Log Out Failed')
+        alert("Log Out Failed");
       }
     });
   };
@@ -29,7 +29,7 @@ function RightMenu(props) {
           <a href="/register">Signup</a>
         </Menu.Item>
       </Menu>
-    )
+    );
   } else {
     return (
       <Menu mode={props.mode}>
@@ -43,8 +43,15 @@ function RightMenu(props) {
 
         <Menu.Item key="cart" style={{ paddingBottom: 3 }}>
           <Badge count={user.userData && user.userData.cart.length}>
-            <a href="/user/cart" className="head-example" style={{ marginRight: -22, color: '#667777' }} >
-              <Icon type="shopping-cart" style={{ fontSize: 30, marginBottom: 3 }} />
+            <a
+              href="/user/cart"
+              className="head-example"
+              style={{ marginRight: -22, color: "#667777" }}
+            >
+              <Icon
+                type="shopping-cart"
+                style={{ fontSize: 30, marginBottom: 3 }}
+              />
             </a>
           </Badge>
         </Menu.Item>
@@ -53,9 +60,8 @@ function RightMenu(props) {
           <a onClick={logoutHandler}>Logout</a>
         </Menu.Item>
       </Menu>
-    )
+    );
   }
 }
 
 export default withRouter(RightMenu);
-
